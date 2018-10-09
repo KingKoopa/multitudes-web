d96d91f098e5db3c0dde12d16ec83d3fa43f0a9f
francisco0689 <francisco.maldonado.campos@gmail.com>
2018-10-09 13:19:36 -0300

    Crea vista Dashboard

--- src/main/java/cl/cleardigital/web/multitudes/controller/DashboardController.java
+++ src/main/java/cl/cleardigital/web/multitudes/controller/DashboardController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import cl.cleardigital.web.multitudes.dto.dashboard.AudienciasPorMesDTO;
 import cl.cleardigital.web.multitudes.service.LeyLobbyService;
@@ -20,6 +21,12 @@ public class DashboardController {
 	@Autowired
 	private LeyLobbyService lobbyService;
 	
+	@RequestMapping(path= {""}, method = RequestMethod.GET)
+	public ModelAndView charts() throws Exception{
+		ModelAndView modelAndView = new ModelAndView("dashboard");
+		return modelAndView;
+	}
+	
 	@RequestMapping(value = "/audiencias-por-mes", method = RequestMethod.GET)
 	public ResponseEntity<?> getAudienciasPorMes(
 			) throws Exception {
@@ -27,6 +34,6 @@ public class DashboardController {
 		
 		return new ResponseEntity<List<AudienciasPorMesDTO>>(audienciasPorMesDTOLst, HttpStatus.OK);	
 	}
-
+	
 	
 }
